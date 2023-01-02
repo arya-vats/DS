b6f69e3c3453688430aa088d4971ad211ea1e36c
arya-vats <aryavats123@gmail.com>
2023-01-02 19:15:21 +0530

    shift-unshift operations

--- linkedLists/linkedList.js
+++ linkedLists/linkedList.js
@@ -46,14 +46,51 @@ class SinglyLinkedList {
         }
         return this; //or another way of doing this was to assign another newTail variable to pointer and keep track of new tail and return simply the pointer.(second option is preferable as it covers more edge cases)
     }
+    shift(){
+        //list is empty
+        if(!this.head){
+            return undefined;
+        }
+        if(this.head.next == null){
+            this.head = null;
+            this.tail = null;
+            return null;
+        }
+        else {
+            this.head = this.head.next;
+        }
+        this.length--;
+        return this;
+    }
+    unshift(val){
+        //adds a new node to the beginning of the list
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        if(!newNode){
+            return undefined;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this.head;
+    }
 }
 
 var list = new SinglyLinkedList();
-console.log(list.push("Hello"));
-console.log(list.push("There!"));
-console.log(list.push(99));
-console.log(list.push(100));
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
+// console.log(list.push("Hello"));
+// console.log(list.push("There!"));
+// console.log(list.push(99));
+// console.log(list.push(100));
+// console.log(list.shift());
+// console.log(list);
+console.log(list.unshift(0));
+console.log(list);
+// console.log(list.tail);
+// console.log(list.head);
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
